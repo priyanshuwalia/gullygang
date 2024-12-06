b03637d5dd90e8c3a2db83489e355500c721fa66
Priyanshu Walia <waliapriyanshu07@gmail.com>
2024-12-06 21:30:23 +0530

    initialed a git repo

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port= 3000;
+app.use(express.json())//
+app.get(`/`, (req,res)=>{
+    console.log("theory will only take you so far")
+    res.json({
+        msg:"hello there"
+    })  
+} );
+app.listen(port, ()=>{
+    console.log(`this is running on port ${port}`)
+});
